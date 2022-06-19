0c33868c20eb2f41c638d9919bbe01ee56004061
Mohamed Ahmed <mowertmn3@gmail.com>
2022-06-19 12:14:38 +0200

    mm

---
+++ DSM.BLL/PEPOLE/BranchBLL.cs
@@ -0,0 +1,40 @@
+﻿using DSM.DAL;
+using DSM.DTO;
+using DSM.TABLES.Guide;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSM.BLL.PEPOLE
+{
+    public class BranchBLL
+    {
+        private readonly IRepository<Branch> branchRepo;
+
+        public BranchBLL(IRepository<Branch> branchRepo)
+        {
+            this.branchRepo = branchRepo;
+        }
+
+
+        #region Get
+
+        public resultDTO GetBranches()
+        {
+            resultDTO result = new resultDTO();
+            result.data = new {
+                Branchs = branchRepo.GetAllAsNoTracking().Select(s => new
+                {
+                    poster = s.Poster,
+                    name = s.Name,
+                    createdDate=s.CreatedDate,
+                    isactive=s.IsActive,
+                })
+            };
+            return result;
+        }
+        #endregion
+    }
+}
---
+++ DSM/Areas/people/controller/BranchController.cs
@@ -0,0 +1,96 @@
+﻿using DSM.BLL.PEPOLE;
+using DSM.DTO;
+using DSM.TABLES.Guide;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace DSM.Areas.people.controller
+{
+    public class BranchController : Controller
+    {
+        private readonly BranchBLL branch;
+
+        public BranchController(BranchBLL branch)
+        {
+            this.branch = branch;
+        }
+        // GET: BranchController
+        public ActionResult Index()
+        {
+            resultDTO r=new resultDTO();
+            var b = branch.GetBranches().data;
+            List<Branch> branches =b.
+            return View();
+        }
+
+        // GET: BranchController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: BranchController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: BranchController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: BranchController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: BranchController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: BranchController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: BranchController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
